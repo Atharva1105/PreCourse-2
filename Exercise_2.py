c6ab1c3fc64e0945ce3825c3969b9bd1382dba29
Atharva Jadhav <atharvajadhav110597@gmail.com>
2024-06-17 19:48:50 -0500

    CompletedEx1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,9 +1,46 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
-def partition(arr,low,high):
-  
-  
+
+def swap(arr, i,j) :
+    tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+
+
+def partition(arr,low,high) :
+
+
+    i,j = low, high
+    pivot = high
+
+
+    while i < j:
+
+        # print(arr[i])
+        # print(arr[pivot])
+
+        while (arr[i] < arr[pivot]) :
+            i += 1
+
+        print(i)
+
+
+        while (arr[j] > arr[pivot]):
+            j -= 1
+
+        if (i<j) :
+
+            arr = swap(arr, i, j)
+
+
+    swap(arr, i, high)
+
+    return i
+
+
+
+
     #write your code here
   
 
@@ -11,6 +48,15 @@ def partition(arr,low,high):
 def quickSort(arr,low,high): 
     
     #write your code here
+
+    if (low < high) :
+
+        i = partition(arr, low, high)
+
+        quickSort(arr, low, i)
+        quickSort(arr, i+1, high)
+
+
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
